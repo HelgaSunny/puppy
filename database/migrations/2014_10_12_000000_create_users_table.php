5021d5f4a7fd7fd82ee08d2ab6aef695224dd053
helgasunn7y2 <sunn7y+helga2@gmail.com>
2019-03-17 14:35:33 +0200

    owner migration, update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->timestamp('last_login')->nullable();
+            $table->ipAddress('last_ip')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
