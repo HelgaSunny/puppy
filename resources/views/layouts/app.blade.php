b0183c6c4aaa450b694982e459357b214e1d80b9
helgasunn7y2 <sunn7y+helga2@gmail.com>
2019-03-19 21:36:55 +0200

    reg and layouts\app sm update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Congratulations! You\'re in!') }}</title>
+    {{--<title>{{ config('app.name', 'Congratulations! You\'re in!') }}</title>--}}
+    <title>{{ config('Pet for weeknd') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +25,8 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Pet for weeknd') }}
+{{--                    {{ config('app.name', 'Pet for weeknd') }}--}}
+                    {{ config('Congratulations! You\'re in!') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
