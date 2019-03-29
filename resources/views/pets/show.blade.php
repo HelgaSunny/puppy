3e94c85e77f3ce7fb131b67ef1581ebcdd41a321
helgasunn7y2 <sunn7y+helga2@gmail.com>
2019-03-29 11:14:55 +0200

    pets buttons auth-bound

--- resources/views/pets/show.blade.php
+++ resources/views/pets/show.blade.php
@@ -3,8 +3,10 @@
 @section('content links')
     Pets SHOW <br>
     @if(!Auth::guest())
-        <a href="/pets/{{$pet->id}}/edit">Edit {{ $pet-> pet_nickname}}'s info</a>
-        <br>
+        @if(Auth::user()->id == $pet->user_id)
+            <a href="/pets/{{$pet->id}}/edit">Edit {{ $pet-> pet_nickname}}'s info</a>
+            <br>
+        @endif
     @endif
 @endsection
 
